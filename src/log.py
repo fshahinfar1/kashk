eafd9624c66876415bf00304063f8859f95ee8ad
Farbod Shahinfar <fshahinfar1@gmail.com>
2023-09-27 16:37:26 +0200

    try to avoid multiple error messages with the same message

--- src/log.py
+++ src/log.py
@@ -1,4 +1,4 @@
-from sys import stderr
+from sys import stderr, stdout
 
 """
 Color Table (16 bit)
@@ -10,15 +10,46 @@ Yellow 	3 	11
 Blue 	4 	12
 Purple 	5 	13
 Cyan 	6 	14
-White 	7 	15 
+White 	7 	15
 """
 
+g_counter = 0
+g_last_line = ''
+
+def is_repeating(args, kwargs):
+    global g_counter
+    global g_last_line
+    sep = ' ' if 'sep' not in kwargs else kwargs['sep']
+    new_line = sep.join(map(str, args))
+    # print(new_line, '||', g_last_line)
+    if new_line == g_last_line:
+        g_counter += 1
+        return True
+    else:
+        g_last_line = new_line
+    return False
+
+
+def clear_repeating():
+    global g_counter
+    global g_last_line
+    if g_counter > 0:
+        print('\n')
+        g_last_line = ''
+    g_counter = 0
+
 def error(*args, **kwargs):
-    print('\033[31m', *args, '\033[0m', file=stderr, **kwargs)
+    if is_repeating(args, kwargs):
+        print(f'\r(x{g_counter}) ', end='', sep='', file=stderr)
+    else:
+        clear_repeating()
+        print('\033[31m', *args, '\033[0m', file=stderr, **kwargs)
 
 
 def debug(*args, **kwargs):
+    clear_repeating()
     print('\033[33m', *args, '\033[0m', file=stderr, **kwargs)
 
 def report(*args, **kwargs):
-    print('\033[36m', *args, '\033[0m', file=stderr, **kwargs)
+    clear_repeating()
+    print('\033[36m', *args, '\033[0m', file=stdout, **kwargs)
