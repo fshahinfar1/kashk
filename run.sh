9deeb924ce5a4c69aaaadf91e7ff6dc34025f453
Farbod Shahinfar <fshahinfar1@gmail.com>
2023-10-10 15:18:05 +0200

    switch to a yaml based config file for gathering better information about the build system

--- run.sh
+++ run.sh
@@ -4,26 +4,36 @@
 if [ -d ./venv/ ]; then
 	source ./venv/bin/activate
 fi
+
 CURDIR=$(dirname $0)
 SCRIPT=$CURDIR/src/main.py
-CASE_STUDY_DIR=$HOME/auto_bpf_offload/auto_bpf_offload_case_study
 
-# FILE=$CASE_STUDY_DIR/src/kv/kv.cpp
+run_with_args() {
+	CASE_STUDY_DIR=$HOME/auto_bpf_offload/auto_bpf_offload_case_study
+	# FILE=$CASE_STUDY_DIR/src/kv/kv.cpp
 
-# FILE=$CASE_STUDY_DIR/src/lookup/lookup.cpp
-# ENTRY=Server::handle_connection
+	# FILE=$CASE_STUDY_DIR/src/lookup/lookup.cpp
+	# ENTRY=Server::handle_connection
 
-# FILE=$CASE_STUDY_DIR/src/twt/twt.cpp
-# ENTRY=WebServer::process_socket
+	# FILE=$CASE_STUDY_DIR/src/twt/twt.cpp
+	# ENTRY=WebServer::process_socket
 
-# FILE=$CASE_STUDY_DIR/src/twt_c/twt_c.c
-# ENTRY=handle_connection
+	# FILE=$CASE_STUDY_DIR/src/twt_c/twt_c.c
+	# ENTRY=handle_connection
 
-FILE=$CASE_STUDY_DIR/src/memcached/memcached.c
-ENTRY=event_handler
-# ARGS="--cflags \-DHAVE_CONFIG_H=1"
+	FILE=$CASE_STUDY_DIR/src/
+	ENTRY=event_handler
+	# ARGS="--cflags \-DHAVE_CONFIG_H=1"
+
+	python3 $SCRIPT $FILE  $ENTRY $ARGS
+	# python3 $SCRIPT $FILE  $ENTRY | clang-format-12
+	# python3 $SCRIPT $FILE  $ENTRY | clang-format-12 | pygmentize -l c
+}
+
+run_with_yaml() {
+	YAML="$CURDIR/mem_config.yaml"
+	python3 $SCRIPT $YAML
+}
 
 
-python3 $SCRIPT $FILE  $ENTRY $ARGS
-# python3 $SCRIPT $FILE  $ENTRY | clang-format-12
-# python3 $SCRIPT $FILE  $ENTRY | clang-format-12 | pygmentize -l c
+run_with_yaml
--- src/main.py
+++ src/main.py
@@ -1,21 +1,55 @@
+import sys
 import argparse
+import yaml
+
+from log import error
 from framework_support import InputOutputContext
 from offload import generate_offload
+
+class Args:
+    def __init__(self):
+        pass
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', help='path to the source file')
+    parser.add_argument('file', help='path to the source file containing the main loop')
     parser.add_argument('func', help='name of the entry function')
+    parser.add_argument('--sources', nargs='*', help='other c files which provide the functions and data-structures')
     parser.add_argument('--out-bpf', help='store generated BPF program in this file', default=None)
     parser.add_argument('--out-user', help='store generated socket program in this file', default=None)
     parser.add_argument('--cflags', help='flags to pass to the compiler', default='')
     args = parser.parse_args()
     return args
 
+def parse_args_yaml():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('yaml_file', help='path to the yaml file containing the project config')
+    args = parser.parse_args()
+    try:
+        with open(args.yaml_file, 'r') as f:
+            config = yaml.load(f)
+    except Exception as e:
+        error('Failed to load yaml file')
+        error(e)
+        sys.exit(1)
+
+    assert 'main' in config, 'The main field must be defined in the yaml file'
+    assert 'entry' in config, 'Them entry field must be defined in the yaml file'
+    obj = Args()
+    obj.file = config['main']
+    obj.func = config['entry']
+    obj.sources = config.get('sources', [])
+    obj.out_bpf = config.get('out_bpf')
+    obj.out_user = config.get('out_user')
+    obj.cflags = config.get('cflags', '')
+    assert isinstance(obj.cflags, str)
+    return obj
+
 
 def main():
-    args = parse_args()
+    # args = parse_args()
+    args = parse_args_yaml()
 
     input_file = args.file
     out_user = args.out_user
