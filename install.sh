3bcf3c7438f652ad36700b23b73d1964ddc8003c
Farbod Shahinfar <fshahinfar1@gmail.com>
2023-11-22 14:37:36 +0100

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 CURDIR=$(realpath $(dirname $0))
-if [ -d $HOME/.bashrc ]; then
+if [ -f $HOME/.bashrc ]; then
 	echo "Found BASH"
-	printf "export KASHK_DIR=$CURDIR" | tee -a ~/.bashrc
+	printf "export KASHK_DIR=$CURDIR\n" | tee -a $HOME/.bashrc
 fi
 
-if [ -d $HOME/.config/fish/config.fish ]; then
+if [ -f $HOME/.config/fish/config.fish ]; then
 	echo "Found Fish"
-	printf "set -x KASHK_DIR $CURDIR" | tee -a ~/.bashrc
+	printf "set -x KASHK_DIR $CURDIR\n" | tee -a $HOME/.config/fish/config.fish
 fi
