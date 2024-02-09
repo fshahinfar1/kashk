89fc456d6692c6e06af07f8f3cadee64ca9f4a2b
Farbod Shahinfar <fshahinfar1@gmail.com>
2024-02-09 07:12:26 -0700

    benchmark: change the server to an echo server

--- cost_benchmark/server.c
+++ cost_benchmark/server.c
@@ -37,9 +37,8 @@ int main(int argc, char *argv[])
 	double avg_cross_time = 0;
 	size_t count = 0;
 	uint64_t acc = 0;
-	struct sockaddr_in sk_addr, addr;
-	socklen_t addr_len;
-	inet_pton(AF_INET, "127.0.0.1", &(sk_addr.sin_addr));
+	struct sockaddr_in sk_addr;
+	inet_pton(AF_INET, "192.168.1.1", &(sk_addr.sin_addr));
 	sk_addr.sin_port = htons(8080);
 	sk_addr.sin_family = AF_INET;
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -48,22 +47,34 @@ int main(int argc, char *argv[])
 	signal(SIGINT, interrupt_handler);
 	printf("Hit Ctrl+C to terminate ...\n");
 	while (running) {
+		struct sockaddr_in addr;
+		socklen_t addr_len = sizeof(addr);
 		int ret;
+		int size;
 		uint64_t user_ts, bpf_ts, travel_time;
 		char buf[BUFSIZE];
-		ret = recvfrom(sock, buf, BUFSIZE, 0,
+		size = recvfrom(sock, buf, BUFSIZE, 0,
 				(struct sockaddr *)&addr, &addr_len);
-		if (ret < 1) {
+		if (size < 1) {
 			continue;
 		}
-		buf[ret] = '\0';
-		printf("recv something: %s\n", buf);
+		buf[size] = '\0';
+		/* printf("recv something: %s\n", buf); */
 		user_ts = get_ns();
 		bpf_ts = ((uint64_t *)buf)[0];
 		travel_time = user_ts - bpf_ts;
 		count++;
 		acc += travel_time;
-		/* Drop everything */
+		/* Echo */
+		/* char *ip = strdup(inet_ntoa(addr.sin_addr)); */
+		/* char *tm = strdup(inet_ntoa(sk_addr.sin_addr)); */
+		/* printf("%s --> %s\n", tm, ip); */
+		ret = sendto(sock, buf, size, 0,
+				(struct sockaddr *)&addr, addr_len);
+		if (ret <= 0) {
+			perror("Failed to echo the message\n");
+			continue;
+		}
 	}
 	if (count == 0) {
 		avg_cross_time = 0;
