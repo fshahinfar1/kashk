72835a47a6f48f2589e273cc14e1c35771154e27
Farbod Shahinfar <fshahinfar1@gmail.com>
2024-02-12 05:10:08 -0700

    fix issue

--- cost_benchmark/benchmarks/echo_sk.c
+++ cost_benchmark/benchmarks/echo_sk.c
@@ -13,6 +13,9 @@ SEC("sk_skb/stream_verdict")
 int verdict(struct __sk_buff *skb)
 {
 	const __u32 zero = 0;
-	return bpf_sk_redirect_map(skb, &sock_map, &zero, 0);
+	/* bpf_printk("here"); */
+	int ret = bpf_sk_redirect_map(skb, &sock_map, zero, 0);
+	/* bpf_printk("ret: %d", ret); */
+	return ret;
 }
 
